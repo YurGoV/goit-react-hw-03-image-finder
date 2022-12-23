91639813533243f723c6e0a2cd2edb4308989b98
YurGoV <yurgov@gmail.com>
2022-12-24 01:29:57 +0200

    stateMachine-draft

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,12 +3,6 @@ import {ButtonStyled} from "./Button.styled";
 
 export const Button = ({loadMore}) => {
 
-  // const onLoadMoreClick = () => {
-  //   console.log('loadmoreclick');
-  //   console.log(page);
-  //   loadMore(page +1);
-  // }
-
   return (
     <ButtonStyled onClick={loadMore}>Load More</ButtonStyled>
   )
--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getImages = async (query, page) => {
-  console.log(query, page);
+export const getImages = async (query, page, perPage) => {
+  // console.log(query, page);
   const response = await axios.get('https://pixabay.com/api/', {
     params: {
       key:'30695501-7cf0afb8f69a77a083ed747e6',
@@ -10,7 +10,7 @@ export const getImages = async (query, page) => {
       orientation: 'horizontal',
       safesearch: true,
       page: page,
-      per_page: 12,
+      per_page: perPage,
     }
   });
   return response.data;
